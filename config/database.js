292ca11eb062042e1d2f4d47217ff66f6ca862f7
eryangboy <450867814@qq.com>
2021-02-01 21:55:15 +0800

    first commit

--- config/database.js
+++ config/database.js
@@ -4,14 +4,30 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "mysql",
-        host: env("DATABASE_HOST", "10.9.162.79"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "blog"),
-        username: env("DATABASE_USERNAME", "root"),
-        password: env("DATABASE_PASSWORD", "yang0622"),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "mysql",
+//         host: env("DATABASE_HOST", "10.9.162.79"),
+//         port: env.int("DATABASE_PORT", 3306),
+//         database: env("DATABASE_NAME", "blog"),
+//         username: env("DATABASE_USERNAME", "root"),
+//         password: env("DATABASE_PASSWORD", "yang0622"),
+//       },
+//       options: {},
+//     },
+//   },
+// });
